b19ae8abda7394f53f86b4ba75f698422dbd5e64
Sathsara Hewage <sathsarahewage@gmail.com>
2025-07-22 23:12:22 +0530

    Refactor App.js to include useEffect for game state management; update physics.js to handle touch events for bird velocity

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { GameEngine } from "react-native-game-engine";
 import entities from "./entities";
